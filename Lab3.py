f99b6bfe8fec5830cc348248ae9dd287123e5e44
mehrad mon <mehrad.mon2000@yahoo.com>
2022-10-16 16:02:17 -0400

    edited lab3.py file to add a function

--- Lab3.py
+++ Lab3.py
@@ -3,6 +3,11 @@ def write(filename):
     f= open(/ops245/lab5/filename, 'w')
     f.write('This is lab3 script, my name is mehrad')
    
+def helloWorld():
+    print('Hello World')
+
+
 if __name__ : '__main__':
     filename= input('What is the filename you want to create? ')
     write(filename)
+    helloWorld()
