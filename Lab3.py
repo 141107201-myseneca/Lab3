706fc51820af22123d2607970280bc46439d0f09
Mehradmontaz <64241878+Mehradmontaz@users.noreply.github.com>
2022-10-16 13:23:38 -0400

    Create Lab3.py

---
+++ Lab3.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+def write(filename):
+    f= open(/ops245/lab5/filename, 'w')
+    f.write('This is lab3 script, my name is mehrad')
+   
+if __name__ : '__main__':
+    filename= input('What is the filename you want to create? ')
+    write(filename)
